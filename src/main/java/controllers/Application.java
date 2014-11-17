7708b181834e744b71d0086d8b21dce12680ff64
Hubert Chen <HubertChen21@gmail.com>
2014-11-16 22:43:57 -0500

    Implemented C3P0 database pooling and cleaned up controllers

--- src/main/java/controllers/Application.java
+++ src/main/java/controllers/Application.java
@@ -1,9 +1,15 @@
 package controllers;
 
+import com.mchange.v2.c3p0.*;
+
+import java.beans.PropertyVetoException;
+import java.sql.*;
+
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 @ComponentScan
 @EnableAutoConfiguration
@@ -12,5 +18,4 @@ public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
-
 }
--- src/main/java/controllers/Main.java
+++ src/main/java/controllers/Main.java
@@ -7,44 +7,63 @@
  */
 package controllers;
 
-import models.User;
+import com.mchange.v2.c3p0.*;
+
+import java.beans.PropertyVetoException;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class Main {
+	private ComboPooledDataSource dataSource;
 
 	@RequestMapping("/")
 	public String index() { return "index"; }
 
 	@RequestMapping(value = "/signup", method = RequestMethod.GET)
-	public String signup(Model model) {
-		model.addAttribute("user", new User());
+	public String signup() {
 		return "signup"; 
 	}
 
-	@RequestMapping(value = "/signup", method = RequestMethod.POST)
-	public String signupSubmit(@ModelAttribute User user, Model model) {
-		model.addAttribute("user", user);
-		User newUser = new User(user.getUsername(), user.getEmail(), user.getPassword());
-		return "result";	
-	}
-
 	@RequestMapping(value = "/signin", method = RequestMethod.GET)
-	public String signin(Model model) {
-		model.addAttribute("user", new User());
+	public String signin() {
 		return "signin"; 
 	}
 
-	@RequestMapping(value = "/signin", method = RequestMethod.GET)
-	public String signinSubmit(@ModelAttribute User user, Model mode) {
-		model.addAttribute("user", user);
-		User.check(user.getUsername(), user.getPassword());
-		return "result2";
+	/* 
+	 * This method is called whenever a Mapping needs to establish
+	 * a connection to the database.
+	 * 
+	 * Will return the datasource if already intialized; otherwise,
+	 * we will create the C3P0 datasource
+	 */
+	private void initDataSource() {
+		if(dataSource == null) { // DataSource has not been intialized
+			String dbUrl 		= "jdbc:postgresql://localhost/Flashcards";
+			String dbUser 		= "test";
+			String dbPassword 	= "123";
+
+			dataSource = new ComboPooledDataSource();
+
+			try {
+				dataSource.setDriverClass("org.postgresql.Driver");
+			} catch (PropertyVetoException e) {
+				System.out.println("ERROR: Unable to set the PostgreSQL JDBC driver");
+				System.out.println("We might be in big trouble Hubert.");
+				System.exit(1);
+			}
+
+			dataSource.setJdbcUrl(dbUrl);
+			dataSource.setUser(dbUser);
+			dataSource.setPassword(dbPassword);
+		} else {
+			/*DataSource has been intialized. Refer to the dataSource
+			 * instance variable.
+			 */
+			return;
+		}
 	}
 }
